c12761a8fa2b0a5386e144024b0fc4c115448f6f
Frank Blau <frank.blau@massiveart.com>
2019-12-03 14:48:34 +0100

    added mailchimp

--- neotest.py
+++ neotest.py
@@ -3,9 +3,43 @@
 from neo4j import GraphDatabase
 import datetime
 import sys
+from mailchimp3 import MailChimp
+import json
+
+def extract_values(obj, key):
+    """Pull all values of specified key from nested JSON."""
+    arr = []
+
+    def extract(obj, arr, key):
+        """Recursively search for values of key in JSON tree."""
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    results = extract(obj, arr, key)
+    return results
+
+
+client = MailChimp(mc_api='98408af2ecb507cdd3ff9e5d173a6b72-us20', mc_user='fjblau@gmail.com')
+campaignData = json.loads(json.dumps(client.reports.get(campaign_id='6032f808ac', get_all=False)))
+
+print(campaignData["list_id"])
+
+listData = json.loads(json.dumps(client.lists.members.all(list_id=campaignData["list_id"])))
+
+print(extract_values(listData,"email_address"))
 
 uri = "bolt://localhost:7687"
 driver = GraphDatabase.driver(uri, auth=("neo4j", "2Ellbelt!"))
+
+
 
 def ts_to_str(ts):
 	return datetime.datetime.fromtimestamp(ts/1000).strftime('%Y-%m-%d %H:%M:%S')
