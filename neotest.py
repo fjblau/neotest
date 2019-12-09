f572b5819cf6cb9c02671c6c5a7f4bd118d56924
Frank Blau <frank.blau@massiveart.com>
2019-12-09 15:31:18 +0100

    working version

--- neotest.py
+++ neotest.py
@@ -1,14 +1,13 @@
-#!/usr/local/opt/python/libexec/bin/python
+#!/usr/bin/python
 
 from neo4j import GraphDatabase
 import datetime
 import sys
 from mailchimp3 import MailChimp
 import json
+import requests
+import hashlib
 
-
-#client = MailChimp(mc_api='98408af2ecb507cdd3ff9e5d173a6b72-us20', mc_user='fjblau@gmail.com')
-#client = MailChimp(mc_api= sys.argv[1], mc_user='fjblau@gmail.com')
 client = MailChimp(mc_api= sys.argv[1], mc_user='accountadmin@massiveart.com')
 
 uri = "bolt://localhost:7687"
@@ -19,6 +18,10 @@ def ts_to_str(ts):
 
 def sq(q):
     return "'" + q + "'"
+
+def hashemailId(c, l, e):
+    hashed = c+l+e
+    return hashlib.sha256(hashed.encode()).hexdigest()
 
 def getAllLists():
     listData = json.loads(json.dumps(client.lists.all(get_all=True)))
@@ -34,7 +37,7 @@ def getAllLists():
             memberId = sq(email["id"])
             createText = """
             MERGE (l:List{listId:"""+listId+", listName:"+listName+"""})
-            MERGE (p:Person{emailAddress:"""+emailAddress+", memberId:"+memberId+", marketing_permissions:"+marketing_permissions+"""})
+            MERGE (p:Person{emailAddress:"""+emailAddress+", memberId:"+memberId+"""})
             MERGE (d:Domain{domain:"""+domain+"""})
             MERGE (p) -[r:MEMBER_OF_LIST]-> (l)
             MERGE (p) -[r2:AT_DOMAIN]-> (d)
@@ -43,7 +46,7 @@ def getAllLists():
         #print(createText)
             with driver.session() as session:
                 result = session.run(createText)
-                print(result)
+                print("Lists", result)
         #memberActivities = json.loads(json.dumps(client.lists.members.activity.all(list_id=listRec["id"], subscriber_hash=email["id"])))
         #print(memberActivities)
 
@@ -52,6 +55,7 @@ def getAllCampaigns():
     campaignData = json.loads(json.dumps(client.campaigns.all(get_all=True)))
     for campaign in campaignData["campaigns"]:
         campaignId = sq(campaign["id"])
+        campaignStatus = campaign["status"]
         emailsSent= campaign["emails_sent"]
         campaignName = sq(campaign["settings"]["title"].replace("'",""))
         campaignRecipientListId = sq(campaign["recipients"]["list_id"])
@@ -66,6 +70,53 @@ def getAllCampaigns():
             result = session.run(createText)
             print(result)
 
+        if (campaignStatus == 'sent'):
+            
+            createText = """
+            MATCH (c:Campaign{campaignId:"""+campaignId+"""})
+            MATCH (l:List{listId:"""+campaignRecipientListId+"""})
+            MERGE (e:EmailGenerated{content:'Email Template'}) - [:GENERATED_BY] -> (c) 
+            MERGE (e2:EmailGenerated {content:'Email Template'}) - [r:SENT_TO] -> (l)
+            """
+            with driver.session() as session:
+                result = session.run(createText)
+                print(result)
+
+        emailActivity = json.loads(json.dumps(client.reports.email_activity.all(campaign_id=campaign["id"], get_all=False)))
+        for email in emailActivity["emails"]:
+            emailHashId = hashemailId(campaign["id"], campaign["recipients"]["list_id"], email["email_address"])
+            emailId = sq(email["email_id"])
+            emailAddress = sq(email["email_address"])
+            createText = """
+            MATCH (p:Person{emailAddress:"""+emailAddress+"""}),
+                  (c2:Campaign {campaignId:"""+campaignId+"""}),
+                  (l2:List{listId:"""+campaignRecipientListId+"""})
+            MERGE (e:Email {content:'Email', status: 'Sent', emailHashId:"""+sq(emailHashId)+"""}) - [to:SENT_TO] -> (p)
+            MERGE (e2:Email{content:'Email', status: 'Sent', emailHashId:"""+sq(emailHashId)+"""}) - [from:SENT_FROM] -> (l2)
+            """
+            with driver.session() as session:
+                result = session.run(createText)
+                print(result)
+            
+            if "activity" in email:
+                for emailAction in email["activity"]:
+                    if emailAction["action"] == 'open':
+                        createText= """
+                                    MATCH (p:Person{emailAddress:"""+emailAddress+"""})
+                                    MATCH (c:Campaign{campaignId:"""+campaignId+"""})
+                                    MERGE (p) - [:OPENED] -> (c)
+                                    """
+                    elif emailAction["action"] == 'bounce':
+                        createText="MATCH(e:Email {emailHashId:"+sq(emailHashId)+"}) SET e.status = 'Bounced'"
+                    elif emailAction["action"] == 'click':
+                        createText= """
+                                    MATCH (p:Person{emailAddress:"""+emailAddress+"""})
+                                    MERGE (u2:URL {url:"""+sq(emailAction["url"])+"""})
+                                    MERGE (p) - [:CLICKED] -> (u2)
+                                    """
+                    with driver.session() as session:
+                        result = session.run(createText)
+                        print(result)
 getAllLists()
 getAllCampaigns()
 
